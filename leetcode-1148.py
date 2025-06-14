3e65d474cf6652c3145141872198a79045ba83a3
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-14 16:04:44 -0700

    Solution for leetcode-1148

---
+++ leetcode-1148.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return (views[views['author_id'] == views['viewer_id']]
+    .drop_duplicates(subset=['author_id'])
+    .sort_values(by = 'author_id', ascending = True)[['author_id']]
+    .rename(columns = {'author_id' : 'id' })
+    )
