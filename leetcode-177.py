e9999aa6cfe6d11a2b3ca506532800539ab45846
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-18 07:22:52 -0700

    Adding alternate methods

--- leetcode-177.py
+++ leetcode-177.py
@@ -11,3 +11,13 @@ def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     nth_sal = candidates[0] if len(candidates) > 0 else None
 
     return pd.DataFrame({f'getNthHighestSalary({N})': [nth_sal]})
+
+# Alternate
+
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    employee['rank'] = employee['salary'].rank(method='dense', ascending=False)
+    nthsal = employee[employee['rank']==N]
+    return pd.DataFrame({f'getNthHighestSalary({N})': [None if len(nthsal) == 0 
+                                                else nthsal['salary'].iloc[0]]})
