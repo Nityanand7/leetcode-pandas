cbd0d9a7c640d74352bbf2fccd18ef208cc69b8d
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-21 11:48:04 -0700

    Adding alternate method to leetcode-196

--- leetcode-196.py
+++ leetcode-196.py
@@ -3,3 +3,12 @@ import pandas as pd
 def delete_duplicate_emails(person: pd.DataFrame) -> None:
     person.sort_values(by='id', inplace=True)
     person.drop_duplicates(subset=['email'], keep = 'first', inplace=True)
+
+# Alternate method - without mutating the order
+
+import pandas as pd
+
+def delete_duplicate_emails(person: pd.DataFrame) -> None:
+    idx = person.groupby('email')['id'].idxmin()
+    to_drop = person.index.difference(idx)
+    person.drop(index=to_drop, inplace=True)
