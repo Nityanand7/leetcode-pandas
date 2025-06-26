9490cfcbda124ed60018843c29185b44b5fd0290
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-26 12:19:58 -0700

    Solution for leetcode-1484

---
+++ leetcode-1484.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    return (
+        activities
+        .groupby('sell_date', as_index=False)
+        .agg(num_sold = ('product', 'nunique'),
+            products = ('product', lambda x: ','.join(sorted(x.unique()))
+                )
+            )
+    )
