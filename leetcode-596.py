6dd635a0efa020b49e4baeb9b02a10ec1f88c7f5
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-25 17:24:56 -0700

    Alternate solution

--- leetcode-596.py
+++ leetcode-596.py
@@ -12,4 +12,12 @@ def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     res = courses.groupby('class').size().reset_index(name='students')
     return res.loc[res['students']>4, ['class']]
 
+#using lambda for filtering
+
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    filtered = courses.groupby('class').filter(lambda grp: len(grp) > 4)
+    return filtered[['class']].drop_duplicates().reset_index(drop=True)
+
 
