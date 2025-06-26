7a51b5f235d12c61b3927b4c9d90e6ead4c87c70
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-25 17:16:54 -0700

    Adding alternate solution

--- leetcode-596.py
+++ leetcode-596.py
@@ -3,3 +3,13 @@ import pandas as pd
 def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
     cnt = courses.groupby('class', as_index=False)['student'].count()
     return cnt.loc[cnt['student']>4, ['class']]
+
+# Using size()
+
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    res = courses.groupby('class').size().reset_index(name='students')
+    return res.loc[res['students']>4, ['class']]
+
+
