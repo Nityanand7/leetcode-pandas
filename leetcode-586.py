28cee14feab7dc834da77543e44e60a4117631cf
Nityanand Madpathi <39838213+Nityanand7@users.noreply.github.com>
2025-06-25 17:53:07 -0700

    Alternate solution

--- leetcode-586.py
+++ leetcode-586.py
@@ -4,3 +4,11 @@ def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
     cnt = orders['customer_number'].value_counts()
     top_cust = cnt[cnt == cnt.max()].index
     return pd.DataFrame({'customer_number': top_cust})
+
+# Alternate way
+
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    order_count = orders.groupby('customer_number').size().reset_index(name='total_orders')
+    return order_count.sort_values('total_orders', ascending=False).head(1)[['customer_number']]
